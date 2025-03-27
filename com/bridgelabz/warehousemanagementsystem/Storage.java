b49ac3a058236aa6b5b3d7d44fb7da96ec3a11c6
Manan Gupta <manangupta27.mg@gmail.com>
2025-03-27 15:17:34 +0530

    [Manan] ADD:Created Storage class to store and print inventory items

---
+++ com/bridgelabz/warehousemanagementsystem/Storage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.warehousemanagementsystem;
+import java.util.*;
+public class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
